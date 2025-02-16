8f6dde84596c568cbea92a40564d371a584ff705
OzeiasMoreira <Ozmoreira17@gmail.com>
2025-02-16 14:52:58 -0300

    #feat : ex de dividir uma listar em numeros pares ou impares

--- lista/ParImpar/main.c
+++ lista/ParImpar/main.c
@@ -6,7 +6,7 @@ typedef struct no{
     struct no *proximo;
 }No;
 
-void inserir_no_incio(No **lista,int num){
+void inserir_no_inicio(No **lista,int num){
     No *novo = malloc(sizeof(No));
 
     if(novo){  
@@ -71,13 +71,101 @@ void inserir_ordenado(No **lista,int num){
             novo->proximo = NULL;
             *lista = novo;
         } else if(novo->valor < (*lista)->valor){
+            novo->proximo = *lista;
+            *lista = novo;
+        } else {
+            aux = *lista;
+            while(aux->proximo && novo->valor > aux->proximo->valor){
+                aux = aux->proximo;
+            }
 
+            novo->proximo = aux->proximo;
+            aux->proximo = novo;
         }
     } else {
         return -1;
     }
 }
 
+No* remover(No **lista, int num){
+    No *aux,*remover = NULL;
+
+    if(*lista){
+        if((*lista)->valor == num){
+            remover = *lista;
+            *lista = remover->proximo;
+        } else {
+            aux = *lista;
+            while(aux->proximo && aux->proximo->valor != num){
+                aux = aux->proximo;
+            }
+
+            if(aux->proximo){
+                remover = aux->proximo;
+                aux->proximo = remover->proximo;
+            }
+        }
+    } else {
+        return -1;
+    }
+
+    return remover;
+}
+
+No* buscar(No **lista,int num){
+    No *aux,*no = NULL;
+
+    aux = *lista;
+
+    while(aux && aux->valor != num){
+        aux = aux->proximo;
+    }
+
+    if(aux){
+        no = aux;
+    }
+
+    return no;
+}
+
+void dividir(No **lista, No **P, No **I){
+    No *aux = *lista;
+    while(aux){
+        if(aux->valor > 0){
+            if(aux->valor %2 == 0){
+                inserir_ordenado(P, aux->valor);
+            } else {
+                inserir_ordenado(I, aux->valor);
+            }
+            aux = aux->proximo;
+        }
+    }
+
+    printf("\n Divisão realizada com sucesso! ");
+}
+
+void imprimir (No *no){
+	printf ("\n Lista: ");
+	
+	while (no){
+		printf ("%d ", no->valor);
+		no = no -> proximo;
+	}
+}
+
+
 int main(){
+    No *lista = NULL;
+    No *par = NULL;
+    No *impar = NULL;
+
+    inserir_ordenado(&lista, 10);
+    inserir_ordenado(&lista, 33);
+    inserir_ordenado(&lista, 13);
+    inserir_ordenado(&lista, 60);
+    dividir(&lista,&par,&impar);
+    imprimir(lista);
+    imprimir(par);
+    imprimir(impar);
     return 0;
 }
